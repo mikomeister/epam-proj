047c6ae856fc449a30246745482337c298932cfa
Rudenko Alexander <a.rudenko@student.csn.khai.edu>
2018-05-29 19:00:19 +0300

    test docker#3

--- docker_run.sh
+++ docker_run.sh
@@ -7,10 +7,12 @@ if [ "$(docker ps -q -f name='test-conteiner')" ]; then
 	docker rmi test-images;
 	echo "container exist";
 	echo "container and image was deleted"
+
+fi
+
 	docker build -t epam-image .
 	docker run -d -p 80:80 --name epam-conteiner epam-image
 	echo "new container was started"
-fi
 
 
 
