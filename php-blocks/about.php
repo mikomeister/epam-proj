d3b5e43bd72ca007672faf8bddc801c514ae8d20
Rudenko Alexander <a.rudenko@student.csn.khai.edu>
2018-05-30 01:54:32 +0300

    add about page #

--- php-blocks/about.php
+++ php-blocks/about.php
@@ -10,7 +10,7 @@
 	</figure>
 	<figure>
 		<p><img class="abt-avatar" src="img/logo.png" alt="avatar"></p>
-		<figurcaption><p>Rudenko Alexander</p><p class="bio">Student of the National Aerospace University Khai, junior web-developer, junior DevOps engineer 
+		<figurcaption><p>Rudenko Alexander</p><p class="bio">Student of the National Aerospace University KhAI, junior web-developer, junior DevOps engineer. 
 		<p class="social-links">
 			<p><img src="img/tg.png" alt="" height="30"><a href="https://t.me/gfmtdkjoi" target="_blank">@gfmtdkjoi</a></p>
 				<p><img src="img/github.png" alt="" height="30"><a href="https://github.com/mikomeister" target="_blank">@mikomeister</a>
