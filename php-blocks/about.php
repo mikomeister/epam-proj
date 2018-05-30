5566473b3aa8776307a3f01826cf33fd735e4fa5
Rudenko Alexander <a.rudenko@student.csn.khai.edu>
2018-05-30 18:40:12 +0300

    add some changes#

--- php-blocks/about.php
+++ php-blocks/about.php
@@ -3,7 +3,7 @@
 		<p><img class="abt-avatar" src="img/logo2.png" alt="avatar"> </p>
 		<figurcaption><p>PhD Oleh Lavrov</p><p class="bio">Researcher of Kharkiv national University of Air Force.</p>
 			<p class="social-links">
-				<p><img src="img/tg.png" alt="" height="30"><a href="https://t.me/gfmtdkjoi" target="_blank">@gfmtdkjoi</a></p>
+				<p><img src="img/tg.png" alt="" height="30"><a href="https://t.me/LavrovOleg" target="_blank">@LavrovOleg</a></p>
 				<p><img src="img/github.png" alt="" height="30"><a href="https://github.com/teler" target="_blank">@teler</a></p>
 			</p>
 </figurcaption>
