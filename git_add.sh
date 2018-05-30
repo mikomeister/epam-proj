6be3b07354da6d2b7e04f9c35d1062d1006f2166
Oleg <lavrov1107@gmail.com>
2018-05-30 18:20:55 +0300

    add new file good.txt

---
+++ git_add.sh
@@ -0,0 +1,7 @@
+cd /media/oleg/hdd/EPAM/epam-proj/
+#git clone git@github.com:mikomeister/epam-proj.git
+#git clone git@github.com:mikomeister/epam-proj.git
+git add --all
+git commit -m "add new file good.txt"
+git remote add origin git@github.com:mikomeister/epam-proj.git
+git push
