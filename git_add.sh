635159117e67eec906a98910c41dde04cd31580b
teler <teler@mail.ru>
2018-05-30 18:29:31 +0300

    Delete git_add.sh

--- git_add.sh
+++
@@ -1,7 +0,0 @@
-cd /media/oleg/hdd/EPAM/epam-proj/
-#git clone git@github.com:mikomeister/epam-proj.git
-#git clone git@github.com:mikomeister/epam-proj.git
-git add --all
-git commit -m "add new file good.txt"
-git remote add origin git@github.com:mikomeister/epam-proj.git
-git push
